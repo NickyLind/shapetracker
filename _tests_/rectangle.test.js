ebd8aac2e8c3a1fa22c2401ce473193533cfab53
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-03 13:20:50 -0800

    pass 2nd good fail

--- _tests_/rectangle.test.js
+++ _tests_/rectangle.test.js
@@ -4,7 +4,6 @@ describe('Rectangle', () =>   {
 
   test('should correctly create a rectangle object using two sides', () => {
     const rectangle = new Rectangle(3,5);
-    expect(rectangle.side1).toEqual(3);
-    expect(rectangle.side2).toEqual(5);
+    expect(rectangle.getArea()).toEqual(15);
   });
 });
